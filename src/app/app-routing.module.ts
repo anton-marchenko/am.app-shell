d69b69f18892fbcfd747ad3b836e580939a3905a
anton-marchenko <fattan@ya.ru>
2023-07-09 12:56:52 +0300

    refactor home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
     path: 'about',
     loadChildren: () =>
       import('./components/about-page/about-page.routes').then(
@@ -21,6 +26,10 @@ const routes: Routes = [
     loadComponent: () => import('mfe1/Component').then((m) => m.AppComponent),
   },
   {
+    // List of apps for iframe integration
+    // might be configured dynamically in backend.
+    // Therefore list of those routes could not be determined in frontend.
+    // Therefore we need to handle by mask all those iframe apps routes:
     path: '**',
     loadComponent: () =>
       import('./components/container/container.component').then(
